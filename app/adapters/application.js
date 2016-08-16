577321bdf0de0d02013c528345e47bbb27b109a7
Kira McLean <kiraemclean@gmail.com>
2016-08-16 11:20:29 -0400

    specify api host url

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,5 +4,6 @@ import Ember from 'ember';
 export default DS.JSONAPIAdapter.extend({
   pathForType: function(type) {
     return Ember.String.pluralize(Ember.String.underscore(type));
-  }
+  },
+  host: 'http://kira-mclean-api.herokuapp.com'
 });
