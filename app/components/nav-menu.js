c1933f691ee43963694f6f50258221a29d92ce17
Kira McLean <kiraemclean@gmail.com>
2016-08-21 09:26:42 -0400

    fix nav on full screen

--- app/components/nav-menu.js
+++ app/components/nav-menu.js
@@ -3,13 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   showMenu: true,
 
-  actions: {
-    toggle() {
-      this.toggleProperty('showMenu')
-    }
-  },
-
-  observer: Ember.observer('showMenu', function() {
-    this.slideDown()
-  })
+  // actions: {
+  //   toggle() {
+  //     this.toggleProperty('showMenu')
+  //   }
+  // },
+  //
+  // observer: Ember.observer('showMenu', function() {
+  //   this.slideDown()
+  // })
 });
